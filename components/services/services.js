b1da0b188fc87fe1bb8aa488445b6c85704a5e21
ITdevJyrgal <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>
2022-03-06 12:32:56 +0600

    index

--- components/services/services.js
+++ components/services/services.js
@@ -1,31 +1,32 @@
 import ServicesDate from "./servicesDate";
 import Image from "next/image"
 import Link from "next/link"
+
 export default function Services() {
 
     return (
 
-            <div className="services">
-                <div style={{display: "flex", alignItems: "center"}}>
-                    <div className="about-general__width"/>
-                    <h1 className="about-general__title">НАШИ УСЛУГИ</h1>
-                    <div className="about-general__width"/>
-                </div>
-                <div className="container">
-                    <div className="services-general">
-                        {
-                            ServicesDate.map(el => (
-                                <Link href={"/services-page"}>
+        <div className="services">
+            <div style={{display: "flex", alignItems: "center"}}>
+                <div className="about-general__width"/>
+                <h1 className="about-general__title">НАШИ УСЛУГИ</h1>
+                <div className="about-general__width"/>
+            </div>
+            <div className="container">
+                <div className="services-general">
+                    {
+                        ServicesDate.map(el => (
+                            <Link href={"/services-page"}>
                                 <div className="services-general__block" key={el.id}>
                                     <Image src={el.img} alt="img" className="services-general__block__img"/>
                                     <p className="services-general__block__item">{el.item}</p>
                                 </div>
-                                </Link>
-                            ))
-                        }
-                    </div>
+                            </Link>
+                        ))
+                    }
                 </div>
             </div>
+        </div>
 
     )
 }
